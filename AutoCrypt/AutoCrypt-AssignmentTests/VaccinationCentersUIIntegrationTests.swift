2e10586e98bc0ee8f0a8445fc029c6b2127a7daf
Sam <klioop2@gmail.com>
2022-10-16 20:34:28 +0900

    `VaccinationCenterListViewController` 는 view 가 로드 된 후 예방접종센터 리스트를 요청한다

--- AutoCrypt/AutoCrypt-AssignmentTests/VaccinationCentersUIIntegrationTests.swift
+++ AutoCrypt/AutoCrypt-AssignmentTests/VaccinationCentersUIIntegrationTests.swift
@@ -6,23 +6,49 @@
 //
 
 import XCTest
+import AutoCrypt_Assignment
 
 final class VaccinationCenterListViewController: UIViewController {
     
+    typealias LoadCompletion = (Result<[VaccinationCenter], RemoteVaccinationCentersLoader.Error>) -> Void
+    
+    var load: ((LoadCompletion) -> Void)?
+    
+    convenience init(load: @escaping (LoadCompletion) -> Void) {
+        self.init()
+        self.load = load
+    }
+    
+    override func viewDidLoad() {
+        self.load? { _ in }
+    }
 }
 
 class VaccinationCentersUIIntegrationTests: XCTestCase {
     
     func test_init_doesNotSendAnyMessage() {
         let loader = LoaderSpy()
-        _ = VaccinationCenterListViewController()
+        _ = VaccinationCenterListViewController(load: loader.load)
         
-        XCTAssertTrue(loader.messages.isEmpty)
+        XCTAssertEqual(loader.loadCallCount, 0)
     }
     
+    func test_viewDidLoad_requestsCenters() {
+        let loader = LoaderSpy()
+        let sut = VaccinationCenterListViewController(load: loader.load)
+
+        sut.loadViewIfNeeded()
+
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
+
     // MARK: - Helpers
     
     private class LoaderSpy {
-        private(set) var messages = [Any]()
+        private(set) var loadCallCount = 0
+        
+        func load(completion: (Result<[VaccinationCenter], RemoteVaccinationCentersLoader.Error>) -> Void) {
+            loadCallCount += 1
+        }
     }
 }
