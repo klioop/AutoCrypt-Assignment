af1ecaf797e5d4263b46d7b0185c32fc307655ce
Sam <klioop2@gmail.com>
2022-10-16 15:55:31 +0900

    헬퍼 함수들을 이용해서 테스트를 리팩토링 - 의미를 명확히 하고 클린한 코드로 수정

--- AutoCript/RemoteAPIEndToEndTests/RemoteAPIEndToEndTests.swift
+++ AutoCript/RemoteAPIEndToEndTests/RemoteAPIEndToEndTests.swift
@@ -11,17 +11,35 @@ import AutoCript_Assignment
 class RemoteAPIEndToEndTests: XCTestCase {
 
     func test_EndToEndServerTest_matchesItemsCountLoadedFromTheSever() {
+        switch getCenterResult() {
+        case let .success(centers):
+            XCTAssertEqual(centers.count, 10)
+            
+        default: break
+        }
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> RemoteVaccinationCentersLoader {
         let url = URL(string: "https://api.odcloud.kr/api/15077586/v1/centers?serviceKey=G%2BNU43EDZdI5nseI67t2beD9Lgaecfc2DFnmHI6419KLzpekWjxDnzhvMajqKVv96rluIxoKv5KSjiZ5%2FICxaw%3D%3D")!
         let client = URLSessionHTTPClient()
         let sut = RemoteVaccinationCentersLoader(url: url, client: client)
+        trackMemoryLeak(client, file: file, line: line)
+        trackMemoryLeak(sut, file: file, line: line)
+        return sut
+    }
+    
+    private func getCenterResult() -> RemoteVaccinationCentersLoader.LoadResult {
+        let sut = makeSUT()
         let exp = expectation(description: "wait for load completion")
         
-        sut.load { result in
-            let centers = try? result.get()
-            XCTAssertEqual(centers?.count, 10)
+        var result: RemoteVaccinationCentersLoader.LoadResult!
+        sut.load { receivedResult in
+            result = receivedResult
             exp.fulfill()
         }
-        
         wait(for: [exp], timeout: 10.0)
+        return result
     }
 }
