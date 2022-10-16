f38f02e00083c53f365891ebb3a4ec00acd16502
Sam <klioop2@gmail.com>
2022-10-16 12:55:34 +0900

    trackMemoryLeak 헬퍼 함수 추가해서 메모리 릭이 발생하는지 테스트

--- AutoCript/AutoCript-AssignmentTests/LoadVaccinationCentersFromRemoteUseCasesTests.swift
+++ AutoCript/AutoCript-AssignmentTests/LoadVaccinationCentersFromRemoteUseCasesTests.swift
@@ -42,11 +42,20 @@ class LoadVaccinationCentersFromRemoteUseCasesTests: XCTestCase {
         XCTAssertEqual(client.requestsURLs, [url])
     }
     
+    // MARK: - Helpers
+    
     private func makeSUT(url: URL = URL(string: "http://any-url.com")!, file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteVaccinationCentersLoader, client: ClientSpy) {
         let client = ClientSpy()
         let sut = RemoteVaccinationCentersLoader(url: url, client: client)
-        
+        trackMemoryLeak(client, file: file, line: line)
+        trackMemoryLeak(sut, file: file, line: line)
         return (sut, client)
+    }
+    
+    private func trackMemoryLeak(_ instance: AnyObject, file: StaticString, line: UInt) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "테스트가 끝나면 \(String(describing: instance)) 는 메모리에서 해제되어야 함. 그렇지 않으면 메모리 릭을 암시", file: file, line: line)
+        }
     }
     
     private class ClientSpy: HTTPClient {
