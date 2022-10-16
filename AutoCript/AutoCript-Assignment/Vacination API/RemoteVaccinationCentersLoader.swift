0ccdad89544d9b9354f3e7c9f5f47dc6b225aa08
Sam <klioop2@gmail.com>
2022-10-16 14:58:35 +0900

    `VaccinationCenter` 도메인 모델을 고유 파일로 옮김

--- AutoCript/AutoCript-Assignment/Vacination API/RemoteVaccinationCentersLoader.swift
+++ AutoCript/AutoCript-Assignment/Vacination API/RemoteVaccinationCentersLoader.swift
@@ -60,31 +60,3 @@ public final class RemoteVaccinationCentersLoader {
         }
     }
 }
-
-public struct VaccinationCenter: Equatable {
-    public let id: CenterID
-    public let name: String
-    public let facilityName: String
-    public let address: String
-    public let lat: String
-    public let lng: String
-    public let updatedAt: String
-    
-    public init(id: CenterID, name: String, facilityName: String, address: String, lat: String, lng: String, updatedAt: String) {
-        self.id = id
-        self.name = name
-        self.facilityName = facilityName
-        self.address = address
-        self.lat = lat
-        self.lng = lng
-        self.updatedAt = updatedAt
-    }
-}
-
-public struct CenterID: Equatable {
-    public let id: Int
-    
-    public init(id: Int) {
-        self.id = id
-    }
-}
