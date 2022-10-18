bce59ee79bb29a59a154bd2b7a418baff4cc52c9
Sam <klioop2@gmail.com>
2022-10-18 18:35:11 +0900

    VaccinationCenterMapper 가 200 http response 와 valid data 에 대해서 에러를  throw 하지 않는 것을 확인

--- AutoCrypt/AutoCrypt-AssignmentTests/API/VaccinationCenterMapperTests.swift
+++ AutoCrypt/AutoCrypt-AssignmentTests/API/VaccinationCenterMapperTests.swift
@@ -20,10 +20,50 @@ class VaccinationCenterMapperTests: XCTestCase {
         }
     }
     
-    func test_map_throwsErrorOnInvalidDataOn200HTTPURLResponse() throws {
+    func test_map_throwsErrorOnInvalidDataWith200HTTPResponse() throws {
         let invalidData = Data("invalid".utf8)
         let response = httpURLResponse(with: 200)
         
         XCTAssertThrowsError(try VaccinationCenterMapper.map(invalidData, from: response))
+    }
+    
+    func test_map_doesNotThrowErrorOnValidDataWith200HTTPResponse() throws {
+        let item = makeItem(updatedAt: "2012-12-12")
+        let validData = makeJSON([item.item])
+        let response = httpURLResponse(with: 200)
+        
+        XCTAssertNoThrow(try VaccinationCenterMapper.map(validData, from: response))
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeItem(id: Int = 0,
+                          name: String = "a name",
+                          facilityName: String = "a facility name",
+                          address: String = "a address",
+                          lat: String = "1.0",
+                          lng: String = "1.0",
+                          updatedAt: String)
+    -> (model: VaccinationCenter, item: [String: Any]) {
+        let model = uniqueCenter(id: id, name: name, facilityName: facilityName, address: address, lat: lat, lng: lng, updatedAt: updatedAt)
+        let item: [String: Any] = [
+            "id": id,
+            "centerName": name,
+            "facilityName": facilityName,
+            "address": address,
+            "lat": "\(lat)",
+            "lng": "\(lng)",
+            "updatedAt": updatedAt,
+        ].compactMapValues { $0 }
+        
+        return (model, item)
+    }
+    
+    private func makeJSON(_ items: [[String: Any]]) -> Data {
+        let json = [
+            "data": items
+        ]
+        
+        return try! JSONSerialization.data(withJSONObject: json)
     }
 }
