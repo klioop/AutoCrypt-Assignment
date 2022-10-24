87a46f220b61c04763f3f1fae155a3b6d8d8cf47
Sam <klioop2@gmail.com>
2022-10-24 19:48:16 +0900

    VaccinationCenterDetailModel 생성

---
+++ AutoCrypt/AutoCrypt-Assignment/Vaccination Feature/VaccinationCenterDetailModel.swift
@@ -0,0 +1,14 @@
+//
+//  VaccinationCenterDetailModel.swift
+//  AutoCrypt-Assignment
+//
+//  Created by klioop on 2022/10/24.
+//
+
+import UIKit
+
+struct VaccinationCenterDetailModel {
+    let image: UIImage?
+    let title: String
+    let description: String
+}
