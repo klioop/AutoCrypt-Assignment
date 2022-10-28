2c98cbb68be795300cded024a3308b82636059f0
Sam <klioop2@gmail.com>
2022-10-28 19:01:41 +0900

    위치서비스 권한 접근이 거절되면 denied 를 전달한다

--- AutoCrypt/AutoCrypt-AssignmentTests/Vaccination Location/LocationAuthorizationServiceTests.swift
+++ AutoCrypt/AutoCrypt-AssignmentTests/Vaccination Location/LocationAuthorizationServiceTests.swift
@@ -6,28 +6,78 @@
 //
 
 import XCTest
+import CoreLocation
 
-final class LocationAuthorizationService {
-    private(set) var completion: (() -> Void)?
+final class LocationAuthorizationService: NSObject, CLLocationManagerDelegate {
+    enum AuthorizationStatus {
+        case denied
+    }
     
-    func start(completion: @escaping () -> Void) {
+    private let manager: CLLocationManager
+    
+    init(manager: CLLocationManager) {
+        self.manager = manager
+    }
+    
+    private(set) var completion: ((AuthorizationStatus) -> Void)?
+    
+    func start(completion: @escaping (AuthorizationStatus) -> Void) {
+        manager.delegate = self
         self.completion = completion
     }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .denied:
+            completion?(.denied)
+            
+        default: break
+        }
+    }
+    
 }
 
 class LocationAuthorizationServiceTests: XCTestCase {
     
     func test_init_configures() {
-        let sut = LocationAuthorizationService()
+        let manager = LocationManagerStub(stubStatus: .denied)
+        let sut = LocationAuthorizationService(manager: manager)
         
         XCTAssertNil(sut.completion)
     }
     
     func test_start_captureCompletion() {
-        let sut = LocationAuthorizationService()
+        let manager = LocationManagerStub(stubStatus: .denied)
+        let sut = LocationAuthorizationService(manager: manager)
         
-        sut.start {}
+        sut.start { _ in }
         
         XCTAssertNotNil(sut.completion)
+    }
+    
+    func test_start_deliversDeniedForLocationServiceIsDenied() {
+        let manager = LocationManagerStub(stubStatus: .denied)
+        let sut = LocationAuthorizationService(manager: manager)
+        
+        let exp = expectation(description: "wait for start completion")
+        sut.start { status in
+            XCTAssertEqual(status, .denied)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    // MARK: - Helpers
+    
+    private class LocationManagerStub: CLLocationManager {
+        let stubStatus: CLAuthorizationStatus
+        
+        init(stubStatus: CLAuthorizationStatus) {
+            self.stubStatus = stubStatus
+        }
+        
+        override var authorizationStatus: CLAuthorizationStatus {
+            stubStatus
+        }
     }
 }
