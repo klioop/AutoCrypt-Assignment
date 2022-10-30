22c6b068a02235e54bdc35ea4f4e2b73c808da20
Sam <klioop2@gmail.com>
2022-10-30 11:56:32 +0900

    VaccinationCenterMapViewModel 의 첫번째 테스트 생성: 생성시 권한 요청을 하지 않는다

---
+++ AutoCrypt/AutoCrypt-AssignmentTests/Vaccination Presentation/VaccinationCenterMapViewModelTests.swift
@@ -0,0 +1,75 @@
+//
+//  VaccinationCenterMapViewModelTests.swift
+//  AutoCrypt-AssignmentTests
+//
+//  Created by klioop on 2022/10/30.
+//
+
+import XCTest
+import AutoCrypt_Assignment
+import CoreLocation
+import RxSwift
+
+final class VaccinationCenterMapViewModel {
+    let start: () -> Single<LocationAuthorizationService.AuthorizationStatus>
+    
+    init(start: @escaping () -> Single<LocationAuthorizationService.AuthorizationStatus>) {
+        self.start = start
+    }
+}
+
+extension LocationAuthorizationService {
+    enum Error: Swift.Error {
+        case unavailable
+        case unRepresented
+    }
+    
+    func start() -> Single<AuthorizationStatus> {
+        Single.create { observer in
+            self.start { status in
+                observer(Result {
+                    switch status {
+                    case .denied, .unavailable:
+                        throw Error.unavailable
+                        
+                    case .unknown:
+                        throw Error.unRepresented
+                        
+                    case .available:
+                        return status
+                    }
+                })
+            }
+            return Disposables.create()
+        }
+    }
+}
+
+class VaccinationCenterMapViewModelTests: XCTestCase {
+    
+    func test_init_doesNotRequestLocationAuthorization() {
+        let manager = CLLocationManager()
+        let service = LocationServiceStub(status: .denied)
+        let sut = VaccinationCenterMapViewModel(start: service.start)
+        
+        XCTAssertEqual(service.startCallCount, 0)
+    }
+    
+    // MARK: - Helpers
+    
+    private class LocationServiceStub {
+        typealias Status = LocationAuthorizationService.AuthorizationStatus
+        var startCallCount = 0
+        
+        let status: Status
+        
+        init(status: Status) {
+            self.status = status
+        }
+        
+        func start() -> Single<Status> {
+            startCallCount += 1
+            return .just(status)
+        }
+    }
+}
